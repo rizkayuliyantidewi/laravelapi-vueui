747932ac6540a158f3bdc579b9ad9d5d289260ab
rizkayuliyantidewi <rizkaydew@gmail.com>
2021-04-01 10:07:39 +0700

    commit 4 pertemuan 4

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,13 +1,13 @@
 <template>
   <div
-    id="carouselExampleCaptions"
+    id="carouselExampleIndicators"
     class="carousel slide"
     data-bs-ride="carousel"
   >
     <div class="carousel-indicators">
       <button
         type="button"
-        data-bs-target="#carouselExampleCaptions"
+        data-bs-target="#carouselExampleIndicators"
         data-bs-slide-to="0"
         class="active"
         aria-current="true"
@@ -15,32 +15,33 @@
       ></button>
       <button
         type="button"
-        data-bs-target="#carouselExampleCaptions"
+        data-bs-target="#carouselExampleIndicators"
         data-bs-slide-to="1"
         aria-label="Slide 2"
       ></button>
       <button
         type="button"
-        data-bs-target="#carouselExampleCaptions"
+        data-bs-target="#carouselExampleIndicators"
         data-bs-slide-to="2"
         aria-label="Slide 3"
       ></button>
     </div>
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="../assets/logo.png" class="d-block w-100" alt="..." />
+        <img src="../assets/7.jpg" class="d-block w-100" alt="..." />
       </div>
       <div class="carousel-item">
-        <img src="../assets/logo.png" class="d-block w-100" alt="..." />
+        <img src="../assets/8.jpg" class="d-block w-100" alt="..." />
         <div class="carousel-caption d-none d-md-block">
       </div>
       <div class="carousel-item">
-        <img src="../assets/logo.png" class="d-block w-100" alt="..." />
+        <img src="../assets/9.jpg" class="d-block w-100" alt="..." />
+      </div>
       </div>
     <button
       class="carousel-control-prev"
       type="button"
-      data-bs-target="#carouselExampleCaptions"
+      data-bs-target="#carouselExampleIndicators"
       data-bs-slide="prev"
     >
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -49,7 +50,7 @@
     <button
       class="carousel-control-next"
       type="button"
-      data-bs-target="#carouselExampleCaptions"
+      data-bs-target="#carouselExampleIndicators"
       data-bs-slide="next"
     >
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
