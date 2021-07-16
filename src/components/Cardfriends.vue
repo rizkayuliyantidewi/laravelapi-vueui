2cfafb188017f53f50a37890a030c282a03abb18
rizkayuliyantidewi <72368819+rizkayuliyantidewi@users.noreply.github.com>
2021-07-16 09:49:52 +0700

    pertemuan 11
    pertemuan 11

--- src/components/Cardfriends.vue
+++ src/components/Cardfriends.vue
@@ -2,13 +2,18 @@
 <div class="row">
  <div class="col-lg-4 mt-3"  v-for="(friend, index) in friends" :key="index">
 <div class="card">
-  <div class="card-header"> {{friend.nama}}</div>
+  <div class="card text-center"> 
+    {{friend.nama}}
+    </div>
   <div class="card-body">
     <h5 class="card-title">{{friend.no_tlp}}</h5>
     <p class="card-text">{{friend.alamat}}</p>
-   
+     <p class="card-text">{{friend.groups.name}}</p>
   </div>
-  <div class="card-footer text-muted"><router-link class="btn btn-primary" :to="{name:'Detailfriends', params:{id:friend.id}}">Detail</router-link>
+  <div class="card-footer text-muted">
+    <router-link class="btn btn-light" :to="{name:'Detailfriends', params:{id:friend.id}}"
+    >Detail</router-link
+    >
   </div>
 </div>
 </div>
